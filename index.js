c7e47db43b2923af82d8f65befb29f60a9f51b8b
Anshu Chaurasia <97455210+the-anshu-dev@users.noreply.github.com>
2024-09-05 16:58:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function greet(name) {
-    return `Hello, ${name}!`;
+  let msg = '';
+  const date = new Date();
+  const hour = date.getHours(); 
+
+ 
+  if (hour >= 0 && hour < 12) {
+    msg = 'Good morning';
+  } else if (hour >= 12 && hour < 18) {
+    msg = 'Good afternoon';
+  } else if (hour >= 18 && hour < 21) {
+    msg = 'Good evening';
+  } else {
+    msg = 'Good night';
   }
-  
-  module.exports = greet;
-  
+
+ 
+  return `${msg}, ${name}!`;
+}
+
+module.exports = greet;
